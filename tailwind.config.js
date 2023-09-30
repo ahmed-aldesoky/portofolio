eb9f9a5ddc8b63f4661a94560d9d397bd76820a0
Ahmed Aldesoky <ahmedaldesoky134@gmail.com>
2023-09-30 17:32:50 +0300

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
    './node_modules/tw-elements/dist/js/**/*.js',
   "./node_modules/flowbite/**/*.js" ],
   theme: {
-    extend: {},
+    extend: {
+           fontFamily: {
+        'ibm': ['IBM Plex Sans'],
+        'ibm-light': ['IBM Plex Sans-light'],
+        'ibm-medium': ['IBM Plex Sans-medium'],
+        'ibm-semiBold': ['IBM Plex Sans-semiBold'],
+        'ibm-bold': ['IBM Plex Sans-bold'],
+
+      }
+    },
   },
   plugins: [
      require('flowbite/plugin')
